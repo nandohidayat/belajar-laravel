bb7a7fa1105112a2f1e332d10ec8c3377c0dd4b7
nandohidayat <mnandohidayat@gmail.com>
2019-09-14 16:07:29 +0700

    add new route, view and try controller

--- part1/resources/views/welcome.blade.php
+++ part1/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mas Bro
                 </div>
 
                 <div class="links">
